924156af024c6062b07fe1f7104ca804d7f98f30
Anton de Groot <adgroot@bkwi.nl>
2026-08-06 10:15:36 +0200

    chore: lower reviews per session

--- src/vault/config.test.ts
+++ src/vault/config.test.ts
@@ -52,7 +52,7 @@ Notes below the frontmatter are ignored.
     expect(parseConfig(written)).toEqual({
       spread: 0.5,
       newPerSession: 8,
-      reviewsPerSession: 15,
+      reviewsPerSession: 10,
       streak: 0,
       lastReviewedOn: "",
       lastSessionOn: "",
